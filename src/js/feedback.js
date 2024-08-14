7d25b790d7899d0156a8f76e9a7ed2ea071c90bd
Danila Odintsov <danila.odintsov01@bk.ru>
2024-08-14 22:24:05 +0400

    add audio logic to feedback cards

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,8 +1,50 @@
 import setupCarousel from './carousel.js'
 
-setupFeedbackSection()
+setupFeedbackCards()
+setupFeedbackCarousel()
 
-function setupFeedbackSection() {
+function setupFeedbackCards() {
+  const cards = document.querySelectorAll('[data-feedback-card]')
+  cards.forEach((card) => setupFeedbackCard(card))
+}
+
+function setupFeedbackCard(cardNode) {
+  const cardAudio = cardNode.querySelector('[data-feedback-audio]')
+  if (!cardAudio) {
+    return
+  }
+
+  cardNode.classList.add('feedback-card_has-audio')
+
+  cardNode.addEventListener('click', () => {
+    if (cardAudio.paused) {
+      pauseActiveAudio()
+      cardAudio.play()
+    } else {
+      cardAudio.pause()
+    }
+  })
+
+  cardAudio.addEventListener('play', () => {
+    cardNode.classList.add('feedback-card_active')
+  })
+
+  cardAudio.addEventListener('pause', () => {
+    cardNode.classList.remove('feedback-card_active')
+  })
+
+  function pauseActiveAudio() {
+    const activeCard = document.querySelector(['[data-feedback-card].feedback-card_active'])
+    if (!activeCard) {
+      return
+    }
+
+    const activeAudio = activeCard.querySelector('[data-feedback-audio]')
+    activeAudio.pause()
+  }
+}
+
+function setupFeedbackCarousel() {
   const carousel = document.querySelector('[data-carousel="feedback"]')
   if (!carousel) return
 
