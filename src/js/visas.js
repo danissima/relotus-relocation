5f62ab1b085a06935787bd5f0b55c7682db89702
Danila Odintsov <danila.odintsov01@bk.ru>
2024-08-14 15:41:04 +0400

    move carousel logic to a separate file

--- src/js/visas.js
+++ src/js/visas.js
@@ -1,4 +1,4 @@
-import EmblaCarousel from 'embla-carousel'
+import setupCarousel from './carousel.js'
 
 setupVisasSection()
 
@@ -6,10 +6,7 @@ function setupVisasSection() {
   const carousel = document.querySelector('[data-carousel="visas"]')
   if (!carousel) return
 
-  const carouselViewport = carousel.querySelector('[data-carousel-viewport]')
   const carouselArrows = document.querySelector('[data-carousel-arrows="visas"]')
-  const carouselButtonPrev = carouselArrows.querySelector('[data-carousel-arrow="prev"]')
-  const carouselButtonNext = carouselArrows.querySelector('[data-carousel-arrow="next"]')
   const carouselSlides = carousel.querySelectorAll('[data-carousel-slide]')
   const isCarouselActive = carouselSlides.length > 4
 
@@ -18,33 +15,16 @@ function setupVisasSection() {
     return
   }
 
-  const carouselApi = EmblaCarousel(carouselViewport, {
-    align: 'start',
-    breakpoints: {
-      '(max-width: 1023px)': {
-        active: false,
+  setupCarousel(
+    carousel,
+    {
+      align: 'start',
+      breakpoints: {
+        '(max-width: 1023px)': {
+          active: false,
+        },
       },
     },
-  })
-
-  carouselOnSelect(carouselApi)
-
-  carouselApi.on('select', carouselOnSelect)
-
-  carouselButtonPrev.addEventListener('click', () => carouselApi.scrollPrev())
-  carouselButtonNext.addEventListener('click', () => carouselApi.scrollNext())
-
-  function carouselOnSelect(api) {
-    if (api.canScrollPrev()) {
-      carouselButtonPrev.removeAttribute('disabled')
-    } else {
-      carouselButtonPrev.setAttribute('disabled', 'disabled')
-    }
-
-    if (api.canScrollNext()) {
-      carouselButtonNext.removeAttribute('disabled')
-    } else {
-      carouselButtonNext.setAttribute('disabled', 'disabled')
-    }
-  }
+    carouselArrows,
+  )
 }
