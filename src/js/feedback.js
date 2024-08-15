b3bbcc95cce419e655a288c3cedc664aaf554f2c
Danila Odintsov <danila.odintsov01@bk.ru>
2024-08-15 13:44:45 +0400

    remove audio logic from feedback card

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -8,41 +8,7 @@ function setupFeedbackCards() {
   cards.forEach((card) => setupFeedbackCard(card))
 }
 
-function setupFeedbackCard(cardNode) {
-  const cardAudio = cardNode.querySelector('[data-feedback-audio]')
-  if (!cardAudio) {
-    return
-  }
-
-  cardNode.classList.add('feedback-card_has-audio')
-
-  cardNode.addEventListener('click', () => {
-    if (cardAudio.paused) {
-      pauseActiveAudio()
-      cardAudio.play()
-    } else {
-      cardAudio.pause()
-    }
-  })
-
-  cardAudio.addEventListener('play', () => {
-    cardNode.classList.add('feedback-card_active')
-  })
-
-  cardAudio.addEventListener('pause', () => {
-    cardNode.classList.remove('feedback-card_active')
-  })
-
-  function pauseActiveAudio() {
-    const activeCard = document.querySelector(['[data-feedback-card].feedback-card_active'])
-    if (!activeCard) {
-      return
-    }
-
-    const activeAudio = activeCard.querySelector('[data-feedback-audio]')
-    activeAudio.pause()
-  }
-}
+function setupFeedbackCard(cardNode) {}
 
 function setupFeedbackCarousel() {
   const carousel = document.querySelector('[data-carousel="feedback"]')
