733285db26cb77851958536affc95dc34380edb0
Danila Odintsov <danila.odintsov01@bk.ru>
2024-08-29 21:52:56 +0400

    add some more anchor links generation cases handling

--- src/js/article.js
+++ src/js/article.js
@@ -3,6 +3,7 @@ setupArticleAnchors()
 function setupArticleAnchors() {
   const article = document.querySelector('[data-article]')
   const anchorsLinks = document.querySelector('[data-article-anchors]')
+  const articleTitle = document.querySelector('[data-article-title]')
 
   if (!article || !anchorsLinks || window.innerWidth < 1024) {
     return
@@ -11,6 +12,16 @@ function setupArticleAnchors() {
   const headings = article.querySelectorAll('h2, h3')
   let currentAnchorId = ''
   const SCROLL_MARGIN_TOP = 106
+
+  if (!headings.length) {
+    const link = createLink(articleTitle.textContent, '#article-content')
+    const li = document.createElement('li')
+
+    li.append(link)
+    anchorsLinks.append(li)
+
+    return
+  }
 
   generateAnchorsLinks()
 
@@ -21,34 +32,54 @@ function setupArticleAnchors() {
     let listItem = null
     let ul = null
 
+    /* if every heading is H3 */
+    /* make ordered links */
+    if (Array.from(headings).every((heading) => heading.tagName === 'H3')) {
+      headings.forEach((heading, index) => {
+        const link = createLink(heading.textContent, `#heading-${index + 1}`)
+        heading.id = `heading-${index + 1}`
+
+        if (!listItem) {
+          listItem = document.createElement('li')
+        }
+
+        listItem.append(link)
+        anchorsLinks.append(listItem)
+        listItem = null
+      })
+
+      return
+    }
+
+    /* if there are H2 and H3 headings */
     headings.forEach((heading, index) => {
+      /* ignore first heading if it's H3 */
       if (heading.tagName === 'H3' && index === 0) {
         return
       }
 
       const nextHeading = headings[index + 1]
 
-      const link = document.createElement('a')
-      link.classList.add('link')
-      link.innerHTML = heading.textContent
-
+      const link = createLink(heading.textContent, `#heading-${index + 1}`)
       heading.id = `heading-${index + 1}`
-      link.href = `#heading-${index + 1}`
 
       if (!listItem) {
         listItem = document.createElement('li')
       }
 
+      /* H2 headings */
       if (heading.tagName === 'H2') {
         listItem.append(link)
 
         if (!nextHeading || nextHeading.tagName === 'H2') {
           anchorsLinks.append(listItem)
+          listItem = null
         }
 
         return
       }
 
+      /* H3 headings */
       if (heading.tagName === 'H3') {
         const listItemSecond = document.createElement('li')
         listItemSecond.append(link)
@@ -91,5 +122,14 @@ function setupArticleAnchors() {
         link.classList.remove('article__anchor_active')
       }
     })
+  }
+
+  function createLink(content, href) {
+    const link = document.createElement('a')
+    link.classList.add('link')
+    link.innerHTML = content
+    link.href = href
+
+    return link
   }
 }
